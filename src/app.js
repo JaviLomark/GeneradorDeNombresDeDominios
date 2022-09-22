29bf5e55727b90a99866ff0e535d2c607297b37a
JaviLomark <lopezlarafranciscojavier@gmail.com>
2022-09-22 17:39:04 +0000

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(
+    "#generadorDeDomnios"
+  ).innerHTML = generadorDeDomnios();
 };
+
+function generadorDeDomnios() {
+  let pronoun = ["Tu", "El"];
+  let noun = ["Programador", "DesarrolladorWeb"];
+  let adj = ["DeConfianza", "Efectivo"];
+  let extension = [".com", ".net", ".pro", ".es"];
+
+  let dominios = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < noun.length; a++) {
+      for (let b = 0; b < adj.length; b++) {
+        for (let c = 0; c < extension.length; c++) {
+          dominios.push(pronoun[i] + noun[a] + adj[b] + extension[c]);
+        }
+      }
+    }
+  }
+  return dominios.join("<br>" + "<br>");
+}
